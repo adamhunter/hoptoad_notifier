9d5f93651f10dd5338dc0da56c37428a5f35385d
Adam Hunter <adamhunter@tma1.com>
2010-04-28 11:27:16 -0400

    updated railtie

--- lib/hoptoad_notifier/railtie.rb
+++ lib/hoptoad_notifier/railtie.rb
@@ -3,13 +3,13 @@ require 'rails'
 
 module HoptoadNotifier
   class Railtie < Rails::Railtie
-    railtie_name :hoptoad_notifier
+    #railtie_name :hoptoad_notifier
 
     rake_tasks do
       require "hoptoad_notifier/rails3_tasks"
     end
 
-    config.middleware.insert_after ActionDispatch::ShowExceptions, HoptoadNotifier::Rack
+    app.middlewares.insert_after ActionDispatch::ShowExceptions, HoptoadNotifier::Rack
 
     config.after_initialize do
       HoptoadNotifier.configure(true) do |config|
